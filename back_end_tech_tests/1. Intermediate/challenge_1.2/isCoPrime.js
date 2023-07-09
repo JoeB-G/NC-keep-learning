2c9def9e5ca6c6929f27fcfeb8c89e45f11123a8
JoeB-G <joeboddenglennon@hotmail.co.uk>
2023-07-09 21:49:13 +0100

    add challenge 1.2 completion

--- back_end_tech_tests/1. Intermediate/challenge_1.2/isCoPrime.js
+++ back_end_tech_tests/1. Intermediate/challenge_1.2/isCoPrime.js
@@ -1,3 +1,10 @@
-const isCoPrime = (num1, num2) => {}
+const isCoPrime = (num1, num2) => {
+    for (let i = 2; i <= Math.min(num1, num2); i++) {
+        if (num1 % i === 0 && num2 % i ===0) {
+            return false
+        }
+    }
+    return true
+}
 
 module.exports = isCoPrime;
