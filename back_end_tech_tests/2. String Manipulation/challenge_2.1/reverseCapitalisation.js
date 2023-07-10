ff453b0763b9eeffe08ccbea1232b39b98ccfb55
JoeB-G <joeboddenglennon@hotmail.co.uk>
2023-07-10 09:27:43 +0100

    add 2.1 solution

--- back_end_tech_tests/2. String Manipulation/challenge_2.1/reverseCapitalisation.js
+++ back_end_tech_tests/2. String Manipulation/challenge_2.1/reverseCapitalisation.js
@@ -1,3 +1,18 @@
-const reverseCapitalisation = () => {}
+const reverseCapitalisation = (string) => {
+  const stringArray = string.split("");
+  const reversedCapitalArray = [];
+
+  for (let i = 0; i < stringArray.length; i++) {
+    if (/[A-Z]/.test(stringArray[i])) {
+      reversedCapitalArray.push(stringArray[i].toLowerCase());
+    } else if (/[a-z]/.test(stringArray[i])) {
+      reversedCapitalArray.push(stringArray[i].toUpperCase());
+    } else {
+      reversedCapitalArray.push(stringArray[i]);
+    }
+  }
+
+  return reversedCapitalArray.join("");
+};
 
 module.exports = reverseCapitalisation;
