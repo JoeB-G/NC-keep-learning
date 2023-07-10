d607272fe1290320f7c999270446ab536bb5027b
JoeB-G <joeboddenglennon@hotmail.co.uk>
2023-07-10 08:53:31 +0100

    add 1.4 solution

--- back_end_tech_tests/1. Intermediate/challenge_1.4/inventory-update.js
+++ back_end_tech_tests/1. Intermediate/challenge_1.4/inventory-update.js
@@ -1,3 +1,22 @@
-function updateInventory() {}
+function updateInventory(items1, items2) {
+    for (let i = 0; i < items1.length; i++) {
+        for (let j = 0; j < items2.length; j++) {
+            if (items1[i][1] == items2[j][1]) {
+                items1[i][0] += items2[j][0]
+                items2.splice(j, 1)
+            }
+        }
+    }
+    
+    const newInventory =  [...items1, ...items2]
+
+    function Comparator(a, b) {
+        if (a[1] < b[1]) return -1;
+        if (a[1] > b[1]) return 1;
+        return 0;
+      }
+
+    return newInventory.sort(Comparator)
+}
 
 module.exports = updateInventory;
